99342febdb6cf3940362a1bf0b3151b648944b82
CastellaFactory <castellafactory@gmail.com>
2016-03-15 22:46:43 +0900

    rebuilding site 2016/03/15 - 22:46

--- deploy.sh
+++ deploy.sh
@@ -27,3 +27,16 @@ git push origin master
 # Come Back
 cd ..
 
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="rebuilding site `date '+%Y/%m/%d - %H:%M'`"
+if [ $# -eq 1 ]
+    then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
